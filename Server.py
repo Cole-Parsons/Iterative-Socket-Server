072137b98078df7743f4c89b317e43bd4ee7e0a3
Surfs-Up5 <cgparsons5@gmail.com>
2026-03-06 16:09:36 -0500

    completed date and time function and working on uptime and testing

---
+++ Server.py
@@ -0,0 +1,28 @@
+#VPN IP: 10.0.96.62
+
+import socket 
+import subprocess
+
+def send_date_and_time(sock):
+    result = subprocess.run(['date'], capture_output=True, text=True)
+    date_time = result.stdout
+    sock.sendall(date_time.encode())
+
+def send_uptime():
+    result = subprocess.run(['uptime'], capture_output=True, text=True)
+    run_time = result.stdout
+    print(run_time)
+    #sock.sendall(run_time.encode())
+
+
+
+print('Starting Iterative Socket Server...')
+port = input('Port: ')
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('0.0.0.0', int(port)))
+server.listen()
+
+send_uptime()
+
+
+
