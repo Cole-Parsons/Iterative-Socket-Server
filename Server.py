3d81a919bc6f5d87eec34258c3afeb6ae9f125fa
Surfs-Up5 <cgparsons5@gmail.com>
2026-03-08 20:56:15 -0400

    added connected user function and added print statements in each function to track successful execution

--- Server.py
+++ Server.py
@@ -1,5 +1,3 @@
-#VPN IP: 10.0.96.62
-
 import socket 
 import subprocess
 
@@ -7,6 +5,7 @@ def send_date_time(sock):
     result = subprocess.run(['date'], capture_output=True, text=True)
     date_time = result.stdout
     sock.sendall(date_time.encode())
+    print('server sent date and time')
 
 def send_uptime(sock):
     result = subprocess.run(['uptime'], capture_output=True, text=True)
@@ -17,12 +16,14 @@ def send_uptime(sock):
     uptime_only = ','.join(words[:2])
 
     sock.sendall(uptime_only.encode())
+    print('server sent uptime')
 
 def send_memory_use(sock):
     result = subprocess.run(['free', '-h'], capture_output=True, text=True)
     memory_line = result.stdout.splitlines()[1]
 
     sock.sendall(memory_line.encode())
+    print('server server sent memory use')
 
 def send_netstats(sock):
     result = subprocess.run(['ss', '-tuln'], capture_output=True, text=True)
@@ -30,6 +31,7 @@ def send_netstats(sock):
     netstat_string = '\n'.join(netstat_lines)
 
     sock.sendall(netstat_string.encode())
+    print('server sent netstats')
 
 def send_running_processes(sock):
     result = subprocess.run(['ps', '-e'], capture_output=True, text=True)
@@ -37,6 +39,12 @@ def send_running_processes(sock):
     running_processes_string = '\n'.join(running_processes)
 
     sock.sendall(running_processes_string.encode())
+    print('server sent running processes')
+
+def send_connected_users(sock):
+    result = subprocess.run(['who'], capture_output=True, text=True)
+    sock.sendall(result.stdout.encode())
+    print('server sent connected users')
 
 print('Starting Iterative Socket Server...')
 port = input('Port: ')
@@ -47,6 +55,7 @@ server.listen()
 while True:
     conn, addr = server.accept()
     client_command = conn.recv(1024).decode().strip().lower()
+    print(f'Client connected from {addr}')
 
     if client_command == 'date and time':
         send_date_time(conn)
@@ -58,7 +67,10 @@ while True:
         send_netstats(conn)
     elif client_command == 'running processes':
         send_running_processes(conn)
+    elif client_command == 'users':
+        send_connected_users(conn)
     else:
         conn.sendall('Invalid option'.encode())
     
     conn.close()
+    print(f'server closed socket connection from {addr}')
